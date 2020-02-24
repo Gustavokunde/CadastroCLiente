559182eb448ad96d4f4d68d1dd45954fc370e9ff
GustavoKunde <gustavokunde@gmail.com>
2020-02-24 20:49:57 -0300

    api finalizada sem validação

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -2,7 +2,7 @@ const User = require("../models/User");
 
 module.exports = {
   async index(req, res) {
-    const users = req.body;
+    const users = await User.find();
 
     return res.json(users);
   },
@@ -12,5 +12,28 @@ module.exports = {
 
     const user = await User.create({ name, email, password });
     return res.json(user);
+  },
+
+  async delete(req, res) {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user._id) {
+      // verifico se o usuario logado é o dono do agendamento
+      return res.status(401).json({
+        error: "Usuário inexistente."
+      });
+    }
+    return res.json(user);
+  },
+
+  async update(req, res) {
+    const user = await User.findByIdAndUpdate(req.params.id, {
+      name: req.body.name,
+      email: req.body.email
+    });
+
+    const newUser = await User.findById(req.params.id);
+
+    return res.json(newUser);
   }
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const UserController = require("./controllers/UserController");
 const routes = express.Router();
 
-routes.get("/clients", UserController.index);
-routes.post("/client", UserController.store);
+routes.get("/", UserController.index);
+routes.post("/clients", UserController.store);
+routes.delete("/clients/:id", UserController.delete);
+routes.put("/clients/:id", UserController.update);
 
 module.exports = routes;
